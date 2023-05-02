466216d00bf2dfc79bba1ef633793602e4f3710b
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2023-05-02 18:32:29 +0800

    works on components

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import GlassPane from "@/components/GlassPane";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-screen w-screen rainbow-mesh p-6">
+      <GlassPane className="w-full h-full flex items-center justify-center">
+        {children}
+      </GlassPane>
+    </div>
+  );
+}
